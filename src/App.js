fc6125aba116b34ce624de9648c8987a59483606
iamthawmus <iamthawmus@gmail.com>
2021-01-18 17:36:54 -0800

    Add steam link

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./styles.css";
 import CenteredTabs from "./CenteredTabs";
 import SimpleAccordion from "./SimpleAccordion";
-import ExperienceData from "./ExperienceData";
-import ProjectData from "./ProjectData";
-import EducationData from "./EducationData";
+import ExperienceData from "./data/ExperienceData";
+import ProjectData from "./data/ProjectData";
+import EducationData from "./data/EducationData";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 export default function App() {
   const [value, setValue] = React.useState(0);
@@ -21,6 +24,14 @@ export default function App() {
       <h2>Welcome to my resume :)</h2>
       <CenteredTabs value={value} handleChange={handleChange} />
       <SimpleAccordion data={data[value]} />
+      {value === 1 ? <Divider /> : null}
+      {value === 1 ? (
+        <Typography>
+          <Link href="https://store.steampowered.com/app/1398420/One_Last_Dungeon/">
+            Visit One Last Dungeon store page
+          </Link>
+        </Typography>
+      ) : null}
     </div>
   );
 }
