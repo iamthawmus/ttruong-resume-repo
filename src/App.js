43b5ce403a934fb795a93dfadda063fa8136f6e7
iamthawmus <iamthawmus@gmail.com>
2021-01-19 16:44:20 -0800

    Added Hobbies Section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EducationData from "./data/EducationData";
 import LinksData from "./data/LinksData";
 import StackData from "./data/StackData";
 import ToolsData from "./data/ToolsData";
+import HobbiesData from "./data/HobbiesData";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -21,8 +22,10 @@ const useStyles = makeStyles((theme) => ({
 export default function App() {
   const classes = useStyles();
 
+  // Keeps track of which tabs are open
   const [value, setValue] = React.useState(0);
   const [value2, setValue2] = React.useState(0);
+  const [value3, setValue3] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -30,11 +33,17 @@ export default function App() {
   const handleChange2 = (event, newValue) => {
     setValue2(newValue);
   };
+  const handleChange3 = (event, newValue) => {
+    setValue3(newValue);
+  };
 
   const data = [ExperienceData, ProjectData, EducationData, LinksData];
   const data2 = [StackData, ToolsData];
+  const data3 = [HobbiesData];
+
   const tabNames = ["Experience", "Projects", "Education", "Links"];
   const tabNames2 = ["Stacks", "Tools"];
+  const tabNames3 = ["Hobbies"];
 
   return (
     <div className={classes.heading}>
@@ -53,6 +62,13 @@ export default function App() {
         tabNames={tabNames2}
       />
       <SimpleAccordion data={data2[value2]} />
+      <h2>About Me</h2>
+      <CenteredTabs
+        value={value3}
+        handleChange={handleChange3}
+        tabNames={tabNames3}
+      />
+      <SimpleAccordion data={data3[value3]} />
     </div>
   );
 }
